3bfc32f874621580d63322f14c2321e8dc3ac9a1
amritranjan <amritranjanamc@gmail.com>
2023-06-20 14:37:40 +0530

    complete db setup for prostgresql

--- config/db.py
+++ config/db.py
@@ -3,7 +3,12 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SQLALCHEMY_DB_URL = "sqlite:///./todos.db?check_same_thread=False"
+# SQLALCHEMY_DB_URL = "sqlite:///./todos.db?check_same_thread=False"
+
+SQLALCHEMY_DB_URL = "postgresql://postgres:root@localhost/TodoApplicationDatabase"
+
+# SQLALCHEMY_DB_URL = "mysql+pymysql://root:root@127.0.0.1:3306/todoapp"
+
 
 engine = create_engine(url=SQLALCHEMY_DB_URL,
                        #    connect_args={
